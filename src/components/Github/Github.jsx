04e5df538a388a59957e138fa7eacce7388d77bc
Arnav Bhargava <83539225+Arnav-Bhargava-cybage@users.noreply.github.com>
2023-10-04 10:06:06 +0530

    final commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  const data = useLoaderData();
+  // const [data, setData] = useState([])
+  // useEffect(() => {
+  //  fetch('https://api.github.com/users/Arnav-Bhargava-cybage')
+  //  .then(response => response.json())
+  //  .then(data => {
+  //     console.log(data);
+  //     setData(data)
+  //  })
+  // }, [])
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNQXIQUES8tg9elQ7leyux3G6_L2OnblZLVZcb8LXWsA&s" alt="Git picture" width={300} />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/Arnav-Bhargava-cybage");
+  return response.json();
+};
